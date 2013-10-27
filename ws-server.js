b3f09b14d5c0bf8596ca10d17dcc0e16bd4dafe7
Frank Lange <langefrq@informatik.hu-berlin.de>
2013-10-27 18:34:03 +0100

    Fixed bugs. Final version!

--- ws-server.js
+++ ws-server.js
@@ -2,16 +2,24 @@ var WebSocketServer = require('ws').Server
 var wss = new WebSocketServer({host: '127.0.0.1',port: 8080});
 
 var cnt = 0;
+var interval_ms = 1000;
 
 function increment() {
   ++cnt;
 }
 
+function sendCounter(ws) {
+  ws.send(cnt.toString());
+}
+
+
 wss.on('connection', function(ws) {
   console.log('client verbunden...');
 
-//  cnt = 0;
-  var interval = setInterval(increment, 1000);
+  var interval1 = setInterval(increment, interval_ms);
+  var interval2 = setInterval(function() {
+    sendCounter(ws);
+  }, interval_ms);
 
 
 });
